25edce8e55b7f2aa9c0b4ea64f180d6cbd202d6e
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-27 16:34:52 +0100

    Creazione di un conto con in ingresso solo il nome

--- src/org/lessons/java/bank/Bank.java
+++ src/org/lessons/java/bank/Bank.java
@@ -1,36 +1,29 @@
 package org.lessons.java.bank;
 
+import java.util.Scanner;
+
 public class Bank {
 
 	public static void main(String[] args) {
 
-		Conto francoBank = new Conto(12, "Franco Rossi");
+		Scanner s = new Scanner(System.in);
 
-		System.out.println("Informazioni del conto di: " + francoBank.accountName);
+		System.out.println("Benvenuto nella creazione del tuo conto bancario!");
+		System.out.print("Inserire nome: ");
+		String inputName = s.nextLine();
+
+		Conto bankAccount = new Conto(inputName);
+
+		System.out.println("---------------------------------------");
+		System.out.println("Riepilogo conto");
 		System.out.println();
-		System.out.println("Codice c/c: " + francoBank.getAccountNumber());
-		System.out.println("Saldo: " + francoBank.formattedBalance() + "€");
+		System.out.println("Nome: " + bankAccount.accountName);
+		System.out.println("Numero conto: " + bankAccount.getAccountNumber());
+		System.out.println("Saldo: " + bankAccount.formattedBalance() + "€");
 
-		System.out.println();
-		System.out.println("--------------------------------------------------------");
-		System.out.println();
+		int choice = 100;
 
-		francoBank.deposit(2000f);
-		System.out.println("Saldo dopo il deposito: " + francoBank.formattedBalance());
-
-		System.out.println();
-
-		boolean firstDone = francoBank.withdraw(15f);
-		if (firstDone)
-			System.out.println("Prelievo eseguito correttamente!");
-		else
-			System.out.println("Impossibile effettuare il prelievo.");
-
-		boolean done = francoBank.withdraw(2000f);
-		if (done)
-			System.out.println("Prelievo eseguito correttamente!");
-		else
-			System.out.println("Impossibile effettuare il prelievo.");
+		s.close();
 
 	}
 

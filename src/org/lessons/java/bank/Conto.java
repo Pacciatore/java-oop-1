7b837084e6b01d2b3e5a7d0f3e9c72ad9d8b3490
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-27 15:32:56 +0100

    Definizione classe Conto e costruttore

--- src/org/lessons/java/bank/Conto.java
+++ src/org/lessons/java/bank/Conto.java
@@ -2,4 +2,20 @@ package org.lessons.java.bank;
 
 public class Conto {
 
+	int accountNumber;
+	String accountName;
+	float balance;
+
+	public Conto(int _accountNumber, String _accountName, float _balance) {
+		this.accountNumber = _accountNumber;
+		this.accountName = _accountName;
+		this.balance = _balance;
+	}
+
+	public Conto(int _accountNumber, String _accountName) {
+		this.accountNumber = _accountNumber;
+		this.accountName = _accountName;
+		this.balance = 0f;
+	}
+
 }

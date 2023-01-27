14987b27fc1b33b585effaa76f555283ca1c4fd5
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-27 14:30:31 +0100

    Creazione costruttore con tutti i parametri passati

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,4 +2,18 @@ package org.lessons.java.shop;
 
 public class Prodotto {
 
+	int id;
+	String name;
+	String description;
+	float price;
+	int vat;
+
+	public Prodotto(int _id, String _name, String _description, float _price, int _vat) {
+		id = _id; // To define getRandomID()
+		name = _name;
+		description = _description;
+		price = _price;
+		vat = _vat;
+	}
+
 }

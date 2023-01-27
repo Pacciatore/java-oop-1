f363bbbe73e58cbd8ecf3d6d0acd310d10b45f00
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-27 14:36:43 +0100

    Print in console del nuovo prodotto base

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -3,7 +3,13 @@ package org.lessons.java.shop;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Prodotto mela = new Prodotto(0, "Mela", "Una bellissima mela rossa.", .6f, 4);
+
+		System.out.println("Ecco il tuo prodotto: " + mela.name);
+		System.out.println("Descrizione: " + mela.description);
+		System.out.println("Prezzo: " + mela.price + "€");
+		System.out.println("IVA: " + mela.vat + "%");
 
 	}
 
